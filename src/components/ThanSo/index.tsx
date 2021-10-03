1ff46e49f12f3bbc568cafe2a840c3540c3f89b6
NamHv <namha@NamHVs-MacBook-Pro.local>
2021-10-04 06:06:30 +0700

    finish tinh toan cac con so

--- src/components/ThanSo/index.tsx
+++ src/components/ThanSo/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Input, DatePicker } from "antd";
 import lodash from "lodash";
 import moment from "moment";
+import { number } from "prop-types";
 
 const ThanSo = () => {
   const dateFormat = "DD/MM/YYYY";
@@ -18,8 +19,17 @@ const ThanSo = () => {
   const [thaiDoBenNgoai, setThaiDoBenNgoai] = useState(null);
   const [dongLucTiepCan, setDongLucTiepCan] = useState(null);
   const arrNghiep = [13, 14, 16, 19];
+  const [namThanSoHienTai, setNamThanSoHienTai] = useState(null);
+  const [namThanSoTiepTheo, setNamThanSoTiepTheo] = useState(null);
+  const arrConSo = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const [noBaiHoc, setNoBaiHoc] = useState([]);
+  const [chiSoCanBang, setChiSoCanBang] = useState(null);
+  const [chiSoPhatTrien, setChiSoPhatTrien] = useState(null);
+  const [thanhPhanNoiTroi, setThanhPhanNoiTroi] = useState([]);
+  const [baGiaiDoan, setBaGiaiDoan] = useState({});
+  const [bonGiaiDoan, setBonGiaiDoan] = useState({});
 
-  const tinhRutGon = (conSoInput: number) => {
+  const tinhRutGon = (conSoInput: number): number => {
     let conSoRutGon = 0;
     if (conSoInput !== 11 && conSoInput !== 22) {
       conSoInput
@@ -150,7 +160,24 @@ const ThanSo = () => {
     }
   };
 
-  const tinhBaiHocDuongDoi = () => {
+  const tinhChiSoNgaySinh = (birthDay) => {
+    if (birthDay) {
+      let thongTinNgaySinh = tinhThongTinNgaySinh();
+      let baGiaiDoan = tinh3GiaiDoan(thongTinNgaySinh.baiHoc);
+      let bonGiaiDoan = tinh4GiaiDoan(thongTinNgaySinh.baiHoc);
+      let namThanSo = tinhNamThanSo(
+        thongTinNgaySinh.ngaySinhRutGon + thongTinNgaySinh.thangSinhRutGon
+      );
+      setBaihoc(thongTinNgaySinh.baiHoc);
+      setNoNghiepBaihocDuongDoi(thongTinNgaySinh.noNghiepDuongDoi);
+      setNamThanSoHienTai(namThanSo.namThanSoHienTai);
+      setNamThanSoTiepTheo(namThanSo.namThanSoTiepTheo);
+      setBaGiaiDoan(baGiaiDoan);
+      setBonGiaiDoan(bonGiaiDoan);
+    }
+  };
+
+  const tinhThongTinNgaySinh = () => {
     let birthDate = birthDay;
     if (birthDate) {
       let arrBirthDate = birthDate.split("/");
@@ -161,26 +188,141 @@ const ThanSo = () => {
       thangSinhRutGon = tinhRutGon(parseInt(arrBirthDate[1]));
       namSinhRutGon = tinhRutGon(parseInt(arrBirthDate[2]));
       let tongNgaySinh = ngaySinhRutGon + thangSinhRutGon + namSinhRutGon;
+      let baiHoc = 0;
+      let noNghiepDuongDoi = "";
       if (tongNgaySinh < 10 || tongNgaySinh === 11 || tongNgaySinh === 22) {
-        setBaihoc(tongNgaySinh);
-        setNoNghiepBaihocDuongDoi("");
+        baiHoc = tongNgaySinh;
       } else if (tongNgaySinh === 10) {
-        setBaihoc(1);
+        baiHoc = 1;
       } else {
         let conSoRutGon2 = 0;
-        let nonghiepDuongDoi = "";
+        // let nonghiepDuongDoi = "";
         tongNgaySinh
           .toString()
           .split("")
           .map((item) => {
             conSoRutGon2 += parseInt(item);
           });
-        nonghiepDuongDoi = tongNgaySinh.toString();
+        noNghiepDuongDoi = tongNgaySinh.toString();
         tongNgaySinh = tinhRutGon(conSoRutGon2);
-        setBaihoc(tongNgaySinh);
-        setNoNghiepBaihocDuongDoi(nonghiepDuongDoi);
+        baiHoc = tongNgaySinh;
       }
+
+      return { noNghiepDuongDoi, baiHoc, ngaySinhRutGon, thangSinhRutGon };
     }
+  };
+
+  const tinhMoc3GiaiDoanBaiHocNhanCach = (baiHocDuongDoi): object => {
+    let giaiDoan1 = 0;
+    let giaiDoan2 = 0;
+    if (baiHocDuongDoi < 5 || baiHocDuongDoi === 11 || baiHocDuongDoi === 22) {
+      giaiDoan1 = 27 - baiHocDuongDoi;
+      giaiDoan2 = giaiDoan1 + 27;
+    } else if (baiHocDuongDoi >= 5 && baiHocDuongDoi <= 9) {
+      giaiDoan1 = 36 - baiHocDuongDoi;
+      giaiDoan2 = giaiDoan1 + 27;
+    } else {
+      giaiDoan1 = 27 - baiHocDuongDoi;
+      giaiDoan2 = giaiDoan1 + 27;
+    }
+    return { giaiDoan1, giaiDoan2 };
+  };
+
+  const tinh4GiaiDoan = (baiHocDuongDoi): object => {
+    // giai doan 1 bang ngay sinh rut gon + thang sinh rut gon
+    // giai doan 2 bang ngay sinh rut gon + nam sinh rut gon
+    // giai doan 3 bang giai doan 1 + giai doan 2
+    // giai doan 4 bang thang sinh rut gon + nam sinh rut gon
+    let arrBirthDay = birthDay.split("/");
+    let ngaySinhRutGon = tinhRutGon(parseInt(arrBirthDay[0]));
+    let thangSinhRutGon = tinhRutGon(parseInt(arrBirthDay[1]));
+    let namSinhRutGon = tinhRutGon(parseInt(arrBirthDay[2]));
+    let giaiDoan1 = tinhRutGon(ngaySinhRutGon + thangSinhRutGon);
+    let giaiDoan2 = tinhRutGon(ngaySinhRutGon + namSinhRutGon);
+    let giaiDoan3 = tinhRutGon(giaiDoan1 + giaiDoan2);
+    let giaiDoan4 = tinhRutGon(thangSinhRutGon + namSinhRutGon);
+    let baiHoc4GiaiDoan = { giaiDoan1, giaiDoan2, giaiDoan3, giaiDoan4 };
+    let mocThoiGian = {
+      giaiDoan1: 36 - baiHocDuongDoi,
+      giaiDoan2: 36 - baiHocDuongDoi + 9,
+      giaiDoan3: 36 - baiHocDuongDoi + 18,
+    };
+    return { baiHoc4GiaiDoan, mocThoiGian };
+  };
+
+  const tinh3GiaiDoan = (baiHocDuongDoi) => {
+    let birthDate = birthDay;
+    if (birthDate) {
+      let arrBirthDay = birthDay.split("/");
+      let giaiDoan1 = tinhRutGon(parseInt(arrBirthDay[1]));
+      let giaiDoan2 = tinhRutGon(parseInt(arrBirthDay[0]));
+      let giaiDoan3 = tinhRutGon(parseInt(arrBirthDay[2]));
+      let baiHocNhanCach3GiaiGoan = { giaiDoan1, giaiDoan2, giaiDoan3 };
+      let moc3GiaiDoan = tinhMoc3GiaiDoanBaiHocNhanCach(baiHocDuongDoi);
+      return { moc3GiaiDoan, baiHocNhanCach3GiaiGoan };
+    }
+    return {};
+  };
+
+  const tinhNoBaiHoc = (fullName) => {
+    let name = convertUnicode(fullName).replaceAll(" ", "");
+    let nameWithNumber = lodash.orderBy(convertNameToNumber(name).split(""));
+    let arrNoBaiHoc = [];
+    arrConSo.map((item) => {
+      if (!lodash.includes(nameWithNumber, item)) {
+        arrNoBaiHoc.push(item);
+      }
+      return item;
+    });
+    return arrNoBaiHoc;
+  };
+
+  const tinhThanhPhanNoiTroi = (fullName) => {
+    let name = convertUnicode(fullName).replaceAll(" ", "");
+    let nameWithNumber = lodash.orderBy(convertNameToNumber(name).split(""));
+    let arrThanhPhanNoiTroi = [];
+    arrConSo.map((item) => {
+      let count = lodash.countBy(nameWithNumber)[item];
+      if (count >= 3) {
+        arrThanhPhanNoiTroi.push(item);
+      }
+      return item;
+    });
+    return arrThanhPhanNoiTroi;
+  };
+
+  const tinhNamThanSo = (baiHocDuongDoi: number) => {
+    var year = moment().year();
+    let namThanSoHienTai = tinhTongDaySoVeMotSo(year + baiHocDuongDoi);
+    let namThanSoTiepTheo = tinhTongDaySoVeMotSo(namThanSoHienTai + 1);
+
+    return { namThanSoHienTai, namThanSoTiepTheo };
+  };
+
+  const tinhChiSoCanBang = (fullName) => {
+    let name = convertUnicode(fullName);
+    let nameWithNumber = lodash.orderBy(convertNameToNumber(name).split(" "));
+    let arrKyTuDauTien = [];
+    nameWithNumber.map((item) => {
+      arrKyTuDauTien.push(item[0]);
+    });
+    let joinStringToNumber = parseInt(arrKyTuDauTien.join(""));
+    let chiSoCanBang = tinhTongDaySoVeMotSo(joinStringToNumber);
+    return chiSoCanBang;
+  };
+
+  const tinhTongDaySoVeMotSo = (number: number) => {
+    let tong = 0;
+    number
+      .toString()
+      .split("")
+      .map((item) => {
+        tong += parseInt(item);
+      });
+    if (tong > 9) {
+      tong = tinhTongDaySoVeMotSo(tong);
+    }
+    return tong;
   };
 
   const kiemTraYPhuAm = (valueCheck) => {
@@ -238,11 +380,18 @@ const ThanSo = () => {
       let nangLucTuNhien = tinhKyTu(fullName);
       let nangLucTiepCan = tinhKyTu(tenRieng);
       let dongLucTiepCan = tinhKyTu(valueTenTieng.nguyenAm);
+      let chiSoCanBang = tinhChiSoCanBang(fullName);
+      let noBaiHoc = tinhNoBaiHoc(fullName);
+      let thanhPhanNoiTroi = tinhThanhPhanNoiTroi(fullName);
       setNangLucTuNhien(nangLucTuNhien);
       setNangLucTiepCan(nangLucTiepCan);
       setThaiDoBenNgoai(thaiDoBenNgoai);
       setDongLucTiepCan(dongLucTiepCan);
       setDongLucThoaMan(dongLucThoaMan);
+      setNoBaiHoc(noBaiHoc);
+      setChiSoCanBang(chiSoCanBang);
+      setChiSoPhatTrien(nangLucTiepCan);
+      setThanhPhanNoiTroi(thanhPhanNoiTroi);
     }
   };
 
@@ -263,9 +412,8 @@ const ThanSo = () => {
   };
 
   const onClickTinhToan = () => {
-    // tinhNangLucTuNhien();
-    tinhBaiHocDuongDoi();
     tinhChiSoTheoTen(fullName);
+    tinhChiSoNgaySinh(birthDay);
   };
 
   const handleOnchangeBirthDay = (value) => {
@@ -286,6 +434,7 @@ const ThanSo = () => {
             defaultValue={moment(new Date(), dateFormat)}
             onChange={(e) => handleOnchangeBirthDay(e)}
             style={{ width: 250 }}
+            format={dateFormat}
           />
         </div>
         <div style={{ marginTop: 15 }}>
@@ -294,7 +443,11 @@ const ThanSo = () => {
             onChange={(e) => handleOnchangeFullName(e.target.value)}
           ></Input>
         </div>
-        <Button style={{ marginTop: 15 }} onClick={() => onClickTinhToan()}>
+        <Button
+          type="primary"
+          style={{ marginTop: 15 }}
+          onClick={() => onClickTinhToan()}
+        >
           Tính Toán
         </Button>
         <div style={{ marginTop: 15 }}>
@@ -321,6 +474,102 @@ const ThanSo = () => {
           <span>{`Động lực thoả mãn: ${
             dongLucThoaMan ? dongLucThoaMan : ""
           }`}</span>
+          <br></br>
+          <span>{`Chỉ số cân bằng: ${chiSoCanBang ? chiSoCanBang : ""}`}</span>
+          <br></br>
+          <span>{`Chỉ số phát triển: ${
+            chiSoPhatTrien ? chiSoPhatTrien : ""
+          }`}</span>
+          <br></br>
+          <span>{`Năm thần số ${moment().year()}: ${
+            namThanSoHienTai ? namThanSoHienTai : ""
+          }`}</span>
+          <br></br>
+          <span>{`Năm thần số ${moment().year() + 1}: ${
+            namThanSoTiepTheo ? namThanSoTiepTheo : ""
+          }`}</span>
+          <br></br>
+          {thanhPhanNoiTroi.length
+            ? thanhPhanNoiTroi.map((item) => {
+                return (
+                  <div key={item}>
+                    <span key={item}>{`Thành phần nổi trội: ${item}`}</span>
+                  </div>
+                );
+              })
+            : null}
+          {/* <br></br> */}
+          {noBaiHoc.length
+            ? noBaiHoc.map((item) => {
+                return (
+                  <div key={item}>
+                    {" "}
+                    <span key={item}>{`Nợ bài học: ${item}`}</span>
+                  </div>
+                );
+              })
+            : null}
+          <br></br>
+          <span>{`Bài học nhân cách từ 0 - ${lodash.get(
+            baGiaiDoan,
+            "moc3GiaiDoan.giaiDoan1"
+          )} tuổi: ${lodash.get(
+            baGiaiDoan,
+            "baiHocNhanCach3GiaiGoan.giaiDoan1"
+          )}`}</span>
+          <br></br>
+          <span>{`Bài học nhân cách từ ${
+            lodash.get(baGiaiDoan, "moc3GiaiDoan.giaiDoan1") + 1
+          } - ${lodash.get(
+            baGiaiDoan,
+            "moc3GiaiDoan.giaiDoan2"
+          )} tuổi: ${lodash.get(
+            baGiaiDoan,
+            "baiHocNhanCach3GiaiGoan.giaiDoan2"
+          )}`}</span>
+          <br></br>
+          <span>{`Bài học nhân cách từ ${
+            lodash.get(baGiaiDoan, "moc3GiaiDoan.giaiDoan2") + 1
+          } - hết tuổi: ${lodash.get(
+            baGiaiDoan,
+            "baiHocNhanCach3GiaiGoan.giaiDoan3"
+          )}`}</span>
+          <br></br>
+          <br></br>
+          <span>{`Bài học kỹ năng từ 1 - ${lodash.get(
+            bonGiaiDoan,
+            "mocThoiGian.giaiDoan1"
+          )} tuổi: ${lodash.get(
+            bonGiaiDoan,
+            "baiHoc4GiaiDoan.giaiDoan1"
+          )}`}</span>
+          <br></br>
+          <span>{`Bài học kỹ năng từ ${
+            lodash.get(bonGiaiDoan, "mocThoiGian.giaiDoan1") + 1
+          } - ${lodash.get(
+            bonGiaiDoan,
+            "mocThoiGian.giaiDoan2"
+          )} tuổi: ${lodash.get(
+            bonGiaiDoan,
+            "baiHoc4GiaiDoan.giaiDoan2"
+          )}`}</span>
+          <br></br>
+          <span>{`Bài học kỹ năng từ ${
+            lodash.get(bonGiaiDoan, "mocThoiGian.giaiDoan2") + 1
+          } - ${lodash.get(
+            bonGiaiDoan,
+            "mocThoiGian.giaiDoan3"
+          )} tuổi: ${lodash.get(
+            bonGiaiDoan,
+            "baiHoc4GiaiDoan.giaiDoan3"
+          )}`}</span>
+          <br></br>
+          <span>{`Bài học kỹ năng từ ${
+            lodash.get(bonGiaiDoan, "mocThoiGian.giaiDoan3") + 1
+          } - hết tuổi: ${lodash.get(
+            bonGiaiDoan,
+            "baiHoc4GiaiDoan.giaiDoan4"
+          )}`}</span>
         </div>
       </div>
     </div>
