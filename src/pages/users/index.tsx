a093b7fb1be4f8b0ef428aaa1f521b3d84d8ac88
MacOne <mac@MacOnes-Mac-mini.local>
2021-10-25 17:11:18 +0700

    build

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,16 +1,16 @@
-import React from "react"
-import { Button } from "antd"
-import ImagePublic from "src/components/ImagePublic"
-import styles from "./user.module.scss"
-import { useRouter } from "next/router"
+import React from "react";
+import { Button } from "antd";
+// import ImagePublic from "src/components/ImagePublic"
+import styles from "./user.module.scss";
+import { useRouter } from "next/router";
 
 const UserPage = () => {
-  const router = useRouter()
+  const router = useRouter();
   const next = () =>
     router.push({
       pathname: "/",
       query: { email: "dinasdas@gmail.com", phone: "123123131212312" },
-    })
+    });
 
   return (
     <div className={styles.users}>
@@ -19,9 +19,9 @@ const UserPage = () => {
       <Button type="primary" onClick={next}>
         Primary Button Antd
       </Button>
-      <ImagePublic src="test.jpeg" alt="Picture of the author" />
+      {/* <ImagePublic src="test.jpeg" alt="Picture of the author" /> */}
     </div>
-  )
-}
+  );
+};
 
-export default UserPage
+export default UserPage;
